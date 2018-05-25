ad6b521443a8befef95a434a597b9dad26d1649d
nishitmehta1 <nishit.mehta1@gmail.com>
2018-05-25 16:27:53 +0530

    Update

--- iris.py
+++ iris.py
@@ -9,8 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 #%%
 data = pd.read_csv('iris.csv')
 data_test = pd.read_csv('iris_test.csv')
@@ -24,15 +23,34 @@ y = data.iloc[:,4].values
 x_test = data_test.iloc[:,:-1].values
 y_test = data_test.iloc[:,4].values
 #%%
-'''
-KNN or NaiveBayes or SVM or Decision Tree or Random Forest
-Which is better?
-More than 2 Classes?
-'''
+from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
+classifier.fit(x, y)
+#%%
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(x, y)
+#%%
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
+classifier.fit(x, y)
+#%%
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(x, y)
+#%%
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
 classifier.fit(x, y)
 #%%
 y_pred = classifier.predict(x_test)
 cm = confusion_matrix(y_test, y_pred)
 accuracy = classifier.score(x_test, y_test)
-print (accuracy*100)
+y_pred = classifier.predict(x_test)
+print (cm)
+print ("Accuracy =", accuracy*100)
+'''
+CLASSIFICATION REPROT
+'''
+target_names = ['Iris-setosa', 'Iris-versicolor','Iris-virginica']
+print(classification_report(y_test, y_pred, target_names= target_names))
